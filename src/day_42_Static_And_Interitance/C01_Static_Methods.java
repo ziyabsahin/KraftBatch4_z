ba9626a82ef4fa94e9358936266b39a5778353c8
ziyabsahin <ziyabuyuksahin@gmail.com>
2023-04-27 21:05:56 +0300

    28

---
+++ src/day_42_Static_And_Interitance/C01_Static_Methods.java
@@ -0,0 +1,13 @@
+package day_42_Static_And_Interitance;
+
+public class C01_Static_Methods {
+    public static void methods(){
+        System.out.println("static");
+    }
+    public void method(){
+        System.out.println("static degil");
+    }
+
+
+
+}
---
+++ src/day_42_Static_And_Interitance/C02_.java
@@ -0,0 +1,17 @@
+package day_42_Static_And_Interitance;
+
+public class C02_ {
+    public static void main(String[] args) {
+        C01_Static_Methods.methods();
+        //statice bir kere giriş yapıyor ve bellege atıyor
+        //yeni bir obje oluştururken ikinci kere staticlere giriş yapmıyor
+        //static degiilse constrocter her cagrıldıgında yüklenir kod blogu
+        //static variable, static blok, constracterı görünce blok, constracter içerisine girer(sıralama bu sekilde)
+        //ikinci obje olusurken de staticlere giriş yapmaz
+        //bir clasın tüm static metodlarını import edip, clas ismi oladan metodu cagırabiliriz
+
+
+
+
+    }
+}
